732aafba157d604eda2d3984f3d4c2e9ea77c8e6
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-05-09 03:23:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const rueda = [
+    ['🐈', '🐕', '🐤', '🐢', '🐏'], 
+    ['🐕', '🐤', '🐈', '🐏', '🐢'], 
+    ['🐤', '🐈', '🐕', '🐢', '🐏']
+]
+
+function mostrar() {
+    for (let i = 1; i <= 3; i++) {
+        for (let j = 1; j <= 3; j++) {
+            const celda = document.getElementById(i + "" + j)
+            celda.innerText = rueda[i - 1][j - 1]
+        }
+    }
+}
+
+function desordenar(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function mover() {
+    desordenar(rueda)
+    desordenar(rueda[0])
+    desordenar(rueda[1])
+    desordenar(rueda[2])
+    mostrar()
+    console.log(rueda);
+}
